2d5685c4876ecb28bbeea089f7911df236a561a8
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-06 07:03:34 +1000

    Add tests for non-batch scaling.

--- pyrr/test/test_matrix.py
+++ pyrr/test/test_matrix.py
@@ -15,6 +15,30 @@ class test_vector( unittest.TestCase ):
         pass
 
     def test_apply_scale( self ):
+        vec = numpy.array(
+            [ 2.0, 2.0, 2.0 ],
+            dtype = numpy.float
+            )
+        scale = ( 10.0, 2.0, 1.0 )
+        scaled = matrix.apply_scale( vec, scale )
+
+        self.assertEqual(
+            scaled[ 0 ],
+            vec[ 0 ] * scale[ 0 ],
+            "Apply scale incorrect"
+            )
+        self.assertEqual(
+            scaled[ 1 ],
+            vec[ 1 ] * scale[ 1 ],
+            "Apply scale incorrect"
+            )
+        self.assertEqual(
+            scaled[ 2 ],
+            vec[ 2 ] * scale[ 2 ],
+            "Apply scale incorrect"
+            )
+
+    def test_batch_apply_scale( self ):
         vecs = numpy.array(
             [
                 [ 2.0, 2.0, 2.0 ],
@@ -45,6 +69,35 @@ class test_vector( unittest.TestCase ):
                 )
 
     def test_apply_direction_scale( self ):
+        vec = numpy.array(
+            [ 2.0, 1.0, 25.0 ],
+            dtype = numpy.float
+            )
+        direction = numpy.array([ 0.0, 0.0, 1.0 ])
+        scaled = matrix.apply_direction_scale(
+            vec,
+            direction,
+            0.0
+            )
+
+        self.assertEqual(
+            scaled[ 0 ],
+            vec[ 0 ],
+            "Scale incorrectly applied"
+            )
+        self.assertEqual(
+            scaled[ 1 ],
+            vec[ 1 ],
+            "Scale incorrectly applied"
+            )
+        self.assertEqual(
+            scaled[ 2 ],
+            0.0,
+            "Scale incorrectly applied"
+            )
+
+
+    def test_batch_apply_direction_scale( self ):
         vecs = numpy.array(
             [
                 [ 0.0, 0.0, 0.0 ],
