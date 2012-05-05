f2c1023d48707cb00d3f4089de88dd33e77b567f
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-06 06:05:40 +1000

    Add testing module.
    All unit tests will be located here in future.

---
+++ pyrr/test/__init__.py
@@ -0,0 +1,24 @@
+import pkgutil
+import os
+
+# make 'from module import *' work dynamically.
+# otherwise we have to manually update the __all__ list.
+# http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python
+for module in os.listdir( os.path.dirname( __file__ ) ):
+    name, extension = os.path.splitext( module )
+
+    # don't import ourself
+    if name == '__init__':
+        continue
+
+    # we can import .py, .pyc and .pyo file types
+    if \
+        extension != '.py' and \
+        extension != '.pyc' and \
+        extension != '.pyo':
+        continue
+
+    # file type is ok, import it
+    __import__( name, locals(), globals() )
+    del module
+
