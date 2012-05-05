d158e7434155c299c6f7736553f725a1e77be8ba
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-06 04:22:54 +1000

    Add missing __init__.py
    Add code to automatically import all .py, .pyc
    and .pyo files inside the module directory.

---
+++ pyrr/__init__.py
@@ -0,0 +1,26 @@
+import pkgutil
+import os
+import pyrr
+
+# make 'from module import *' work dynamically.
+# otherwise we have to manually update the __all__ list.
+# http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python
+for module in os.listdir( os.path.dirname( __file__ ) ):
+    name, extension = os.path.splitext( module )
+
+    # don't import ourself
+    if name == '__init__':
+        print 'found init %s' % module
+        continue
+
+    # we can import .py, .pyc and .pyo file types
+    if \
+        extension != '.py' and \
+        extension != '.pyc' and \
+        extension != '.pyo':
+        continue
+
+    # file type is ok, import it
+    __import__( name, locals(), globals() )
+    del module
+
