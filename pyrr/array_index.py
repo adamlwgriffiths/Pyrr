44f2b6474b16a0c2c96e49cac33f3e3fe9466772
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-07 06:26:13 +1000

    Clean up array index

--- pyrr/array_index.py
+++ pyrr/array_index.py
@@ -10,30 +10,42 @@ def array_3d_position_to_index( size, position ):
     1D array
     """
     # index = ( (x * ySize) + y) * zSize + z
-    return (((position[ 0 ] * size[ 1 ]) + position[ 1 ]) * size[ 2 ]) + position[ 2 ]
+    x = position[ 0 ]
+    y = position[ 1 ]
+    z = position[ 2 ]
+    y_size = size[ 1 ]
+    z_size = size[ 2 ]
+    return (((x * y_size) + y) * z_size) + z
 
 def array_3d_index_to_position( size, index ):
     """
     Converts an array index into a 3D position
     """
+    y_size = size[ 1 ]
+    z_size = size[ 2 ]
+
     i = index
     
-    x = i / (size[ 1 ] * size[ 2 ])
-    i -= (x * (size[ 1 ] * size[ 2 ]))
+    x = i / (y_size * z_size)
+    i -= (x * (y_size * z_size))
     
-    y = i / size[ 2 ]
+    y = i / z_size
     
-    z = i - (y * size[ 2 ])
+    z = i - (y * z_size)
     
     return (x, y, z)
 
-def array_3d_index_increment_x_unsafe( size, index, increment ):
+def array_3d_index_increment_unsafe( size, index, increment ):
     """
     Takes a position and quickly shuffles along an axis.
     This avoids having to calculate the index many times for each vertex.
     This function does not do bounds checking for speed. 
+
+    @param size: the dimensions of the container as a 3d vector.
+    @param index: the existing index value.
+    @param increment: a 3d vector to increment the position by.
     """
-    # TODO:
+    #index += increment[ 0 ] * size[ 1 ]
     pass
 
 
