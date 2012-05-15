906af6670f3fb1b64f1304d8aa0d769e4e9769dc
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-15 13:23:59 +1000

    Update aabb tests.
    Split existing add_points test into 2 tests.
    One is now for create_from_points.
    Add test for create_from_aabbs.

--- pyrr/test/test_aabb.py
+++ pyrr/test/test_aabb.py
@@ -14,7 +14,7 @@ class test_ray( unittest.TestCase ):
     def tearDown( self ):
         pass
 
-    def test_add_point( self ):
+    def test_create_from_points( self ):
         obj = aabb.create_from_points(
             numpy.array(
                 [[-1.0,-1.0,-1.0]],
@@ -23,15 +23,59 @@ class test_ray( unittest.TestCase ):
             )
         self.assertTrue(
             numpy.array_equal( obj[ 0 ], [-1.0,-1.0,-1.0 ] ),
-            "Add point failed"
+            "Create from points failed"
             )
         self.assertTrue(
             numpy.array_equal( obj[ 1 ], [-1.0,-1.0,-1.0 ] ),
-            "Add point failed"
+            "Create from points failed"
             )
         self.assertTrue(
             numpy.array_equal( aabb.centre_point(obj), [-1.0,-1.0,-1.0 ] ),
-            "Add point failed"
+            "Create from points failed"
+            )
+
+    def test_create_from_aabbs( self ):
+        a1 = aabb.create_from_points(
+            [
+                [ 0.0, 0.0, 0.0 ],
+                [ 1.0, 1.0,-1.0 ]
+                ]
+            )
+        a2 = aabb.create_from_points(
+            [
+                [ 0.0, 0.0, 0.0 ],
+                [-1.0,-1.0, 1.0 ]
+                ]
+            )
+        a3 = aabb.create_from_aabbs(
+            numpy.array(
+                [ a1, a2 ]
+                )
+            )
+
+        self.assertTrue(
+            numpy.array_equal(
+                a3[ 0 ],
+                [-1.0,-1.0,-1.0 ]
+                ),
+            "Create from AABBS failed"
+            )
+        self.assertTrue(
+            numpy.array_equal(
+                a3[ 1 ],
+                [ 1.0, 1.0, 1.0 ]
+                ),
+            "Create from AABBS failed"
+            )
+
+
+    def test_add_point( self ):
+        obj = numpy.array(
+            [
+                [-1.0,-1.0,-1.0],
+                [-1.0,-1.0,-1.0]
+                ],
+            dtype = numpy.float
             )
 
         aabb.add_points(
