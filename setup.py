728ad354f83057de04a2788154da4cef6cb7a7af
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-08-16 15:52:49 +1000

    Rename Pyrr to pyrr in setup.py, add requires.

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ from distutils.core import setup
 import pyrr
 
 setup(
-    name = 'Pyrr',
+    name = 'pyrr',
     version = pyrr.__version__,
     description = '3D mathematical functions using NumPy',
     license = 'BSD',
     author = 'Adam Griffiths',
     author_email = 'adam.lw.griffiths@gmail.com',
     url = 'https://github.com/adamlwgriffiths/Pyrr',
+    requires = [
+        'numpy',
+        ],
     platforms = [ 'any' ],
     test_suite = "pyrr.test",
     packages = [
