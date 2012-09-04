515d64f67c92e345067d0198f98f0b3b35f05421
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-09-04 13:46:44 +1000

    Add commenting to AABB

--- pyrr/aabb.py
+++ pyrr/aabb.py
@@ -1,10 +1,12 @@
-'''
-Created on 09/04/2012
+""" Provides functions to calculate and manipulate
+Axis-Aligned Bounding Boxes.
 
-@author: adam
+An AABB is represented by an array of 2 x 3D vectors.
+The first vector represents the minimum extent.
+The second vector represents the maximum extent.
 
 TODO: add transform( matrix )
-'''
+"""
 
 import numpy
 
@@ -13,12 +15,23 @@ def _empty():
     return numpy.empty( (2,3), dtype = numpy.float )
 
 def create_from_bounds( min, max ):
+    """ Creates an AABB using the specified minimum
+    and maximum values.
+    """
     return numpy.array(
         [ min, max ],
         dtype = numpy.float
         )
 
 def create_from_points( points, out = None ):
+    """ Creates an AABB from the list of specified points.
+
+    Points must be a 2D list. Ie:
+    [
+        [ x, y, z ],
+        [ x, y, z ],
+    ]
+    """
     if out == None:
         out = _empty()
 
@@ -27,6 +40,14 @@ def create_from_points( points, out = None ):
     return out
 
 def create_from_aabbs( aabbs, out = None ):
+    """ Creates an AABB from a list of existing AABBs.
+
+    AABBs must be a 2D list. Ie:
+    [
+        AABB,
+        AABB,
+    ]
+    """
     if out == None:
         out = _empty()
 
@@ -36,6 +57,9 @@ def create_from_aabbs( aabbs, out = None ):
     return out
 
 def add_points( aabb, points, out = None ):
+    """ Extends an AABB to encompass a list
+    of points.
+    """
     if out == None:
         out = _empty()
 
@@ -50,37 +74,61 @@ def add_points( aabb, points, out = None ):
     return out
 
 def add_aabbs( aabb, aabbs, out = None ):
+    """ Extend an AABB to encompass a list
+    of other AABBs.
+    """
     if out == None:
         out = _empty()
 
+    # extract the AABB's minimum and maximum
     minimum = numpy.amin( aabbs[:, 0], axis = 0 )
     maximum = numpy.amax( aabbs[:, 1], axis = 0 )
 
+    # compare to our existing AABB
     numpy.minimum( aabb[ 0 ], minimum, out = out[ 0 ] )
     numpy.maximum( aabb[ 1 ], maximum, out = out[ 1 ] )
     return out
 
 def centre_point( aabb ):
+    """ Returns the centre point of the AABB.
+    """
     return (aabb[ 0 ] + aabb[ 1 ]) * 0.5
 
 def minimum( aabb ):
+    """ Returns the minimum point of the AABB.
+    """
     return aabb[ 0 ]
 
 def maximum( aabb ):
+    """ Returns the maximum point of the AABB.
+    """
     return aabb[ 1 ]
 
 def clamp_points( aabb, points, out = None ):
+    """ Takes a list of points and modifies them to
+    fit within the AABB.
+    """
     if out == None:
         out = numpy.empty( 3, dtype = numpy.float )
+
+    # we need to compare the points against our AABB.
+    # minimum( point, AABB maximum )
+    # maximum( point, AABB minimum )
 
     # clamp the point by getting the maximum of the
     # point and the AABB's minimum
     # then the minimum of the point and the AABB's
     # maximum
     if points.ndim == 1:
+        # only a single point
+        # just take the existing AABB for comparisson
         aabb_min = aabb[ 0 ]
         aabb_max = aabb[ 1 ]
     else:
+        # there are multiple points
+        # so we'll repeat our AABB values for easy
+        # comparison
+
         # use a stride trick to repeat the AABB arrays
         # without actually allocating any data
         # http://stackoverflow.com/questions/5564098/repeat-numpy-array-without-replicating-data
@@ -94,6 +142,7 @@ def clamp_points( aabb, points, out = None ):
             (points.shape[ 0 ], aabb[ 1 ].size),
             (0, aabb[ 1 ].itemsize)
             )
+
     numpy.maximum( points, aabb_min, out = out[ 0 ] )
     numpy.minimum( points, aabb_max, out = out[ 1 ] )
 
