9afbffa0ff3b69b0c04ff502df94b24f6f8183eb
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-11-25 12:14:37 +1100

    Improve matrix44 tests.

--- pyrr/test/test_matrix44.py
+++ pyrr/test/test_matrix44.py
@@ -3,6 +3,7 @@ import math
 
 import numpy
 
+from pyrr import matrix33
 from pyrr import matrix44
 from pyrr import quaternion
 
@@ -16,24 +17,26 @@ class test_matrix44( unittest.TestCase ):
         pass
 
     def test_identity( self ):
-        mat = matrix44.identity()
+        result = matrix44.identity()
+
+        expected = numpy.eye( 4 )
 
         self.assertTrue(
-            numpy.array_equal(
-                mat,
-                numpy.eye( 4 )
-                ),
-            "Not an identity matrix"
+            numpy.array_equal( result, expected ),
+            "Matrix44 identity incorrect"
             )
 
     def test_create_from_translation( self ):
-        translation = numpy.array( [ 1.0, 2.0, 3.0 ])
+        translation = numpy.array( [ 2.0, 3.0, 4.0 ] )
         mat = matrix44.create_from_translation( translation )
+        result = mat[ 3, 0:3 ]
+
+        expected = translation
 
         # translation goes down the last column in normal matrix
         self.assertTrue(
-            numpy.array_equal( mat[ 3, 0:3 ], translation ),
-            "Translation not set properly"
+            numpy.array_equal( result, expected ),
+            "Matrix44 translation not set properly"
             )
 
     def test_create_from_scale( self ):
@@ -41,31 +44,49 @@ class test_matrix44( unittest.TestCase ):
 
         mat = matrix44.create_from_scale( scale )
 
+        result = mat.diagonal()[ :-1 ]
+
+        expected = scale
+
         # extract the diagonal scale and ignore the last value
         self.assertTrue(
-            numpy.array_equal( mat.diagonal()[ :-1 ], scale ),
-            "Scale not set properly"
+            numpy.array_equal( result, expected ),
+            "Matrix44 scale not set properly"
             )
 
     def test_to_matrix33( self ):
         mat = matrix44.identity()
-        mat = matrix44.to_matrix33( mat )
+        result = matrix44.to_matrix33( mat )
+
+        expected = numpy.eye( 3 )
 
         self.assertTrue(
-            numpy.array_equal( mat, numpy.eye( 3 ) ),
-            "Matrix33 not extracted properly"
+            numpy.array_equal( result, expected ),
+            "Matrix44 to_matrix33 incorrect"
+            )
+
+    def test_create_from_matrix33( self ):
+        mat = matrix33.identity()
+        result = matrix44.create_from_matrix33( mat )
+
+        expected = numpy.eye( 4 )
+
+        self.assertTrue(
+            numpy.array_equal( result, expected ),
+            "Matrix33 to_matrix44 incorrect"
             )
 
     def test_create_from_quaternion( self ):
         # identity
         quat = quaternion.identity()
-        mat = matrix44.create_from_quaternion( quat )
+        result = matrix44.create_from_quaternion( quat )
+
+        expected = numpy.eye( 4 )
 
         self.assertTrue(
-            numpy.array_equal( mat, numpy.eye( 4 ) ),
-            "Quaternion to Matrix conversion failed with identity quaternion"
+            numpy.array_equal( result, expected ),
+            "Matrix44 from quaternion incorrect with identity quaternion"
             )
     
 if __name__ == '__main__':
     unittest.main()
-
