cf44836ab04e856d56e9f5ef37c25eaf4f4313fb
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-11-25 12:20:24 +1100

    Add eulers module.
    Currently only supports creation.
    This is really to help show the order they are
    stored in for usage in matrix33/44 methods.

---
+++ pyrr/eulers.py
@@ -0,0 +1,19 @@
+import numpy
+
+
+class index:
+    pitch = 0
+    roll = 1
+    yaw = 2
+
+def create( pitch, roll, yaw ):
+    return numpy.array( [ pitch, roll, yaw ] )
+
+def pitch( eulers ):
+    return eulers[ 0 ]
+
+def roll( eulers ):
+    return eulers[ 1 ]
+
+def yaw( eulers ):
+    return eulers[ 2 ]
