4d055688e5856e500c2f778e0da6695735330088
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-12-03 07:20:08 +1100

    Add utility decorators
    These decorators are used to ensure that
    parameters are converted to numpy.arrays.
    This can prevent some accidental misusage that
    won't trigger errors, such as adding 2 lists.
    Adding lists in numpy performs addition, but for
    python lists it concatenates.

---
+++ pyrr/utils.py
@@ -0,0 +1,53 @@
+import inspect
+import numpy
+
+def all_parameters_as_numpy_arrays( fn ):
+    """Converts all of a function's arguments to numpy arrays.
+    """
+    def wrapper( *args, **kwargs ):
+        np_args = [ numpy.array( arg ) for arg in args ]
+        np_kwargs = { key: numpy.array( value ) for (key, value) in kwargs }
+        return fn( *np_args, **np_kwargs )
+    return wrapper
+
+
+def parameters_as_numpy_arrays( *args_to_convert ):
+    """Converts specific arguments to numpy arrays.
+
+    Arguments are specified by their argument name.
+    Ie.
+    @parameters_as_numpy_arrays( 'a', 'b', 'optional' )
+    def myfunc( a, b, *args, **kwargs ):
+        pass
+
+    myfunc( 1, [2,2], optional = [3,3,3] )
+    """
+    def decorator( fn ):
+        def wrapper( *args, **kwargs ):
+            # get the arguements of the function we're decorating
+            fn_args = inspect.getargspec( fn )
+
+            # convert any values that are specified
+            # if the argument isn't in our list, just pass it through
+
+            # convert the *args list
+            # we zip the args with the argument names we received from
+            # the inspect function
+            np_args = [
+                numpy.array( value )
+                if key in args_to_convert
+                else value
+                for key, value in zip( fn_args.args, args )
+                ]
+
+            # convert the **kwargs dict
+            np_kwargs = {
+                key:
+                (numpy.array( value ) if key in args_to_convert else value)
+                for key, value in kwargs.items()
+                }
+
+            # pass the converted values to our function
+            return fn( *np_args, **np_kwargs )
+        return wrapper
+    return decorator
