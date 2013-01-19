b3d8501a25512ee5a70df888d86e1a250785d008
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-01-19 18:10:10 +1100

    Add create_from_matrix44_translation.
    Extracts the translation from a matrix44.

--- pyrr/vector3.py
+++ pyrr/vector3.py
@@ -16,6 +16,9 @@ def create_unit_length_z():
 def create_from_vector4( vector ):
     return numpy.array( vector[ :-1 ] )
 
+def create_from_matrix44_translation( mat ):
+    return mat[ 3, 0:3 ].copy()
+
 class unit:
     x = create_unit_length_x()
     y = create_unit_length_y()
--- pyrr/vector4.py
+++ pyrr/vector4.py
@@ -16,6 +16,9 @@ def create_unit_length_z():
 def create_from_vector3( vector ):
     return numpy.array( [ vec[0], vec[1], vec[2], 1.0 ] )
 
+def create_from_matrix44_translation( mat ):
+    return mat[ 3, 0:4 ].copy()
+
 class unit:
     x = create_unit_length_x()
     y = create_unit_length_y()
