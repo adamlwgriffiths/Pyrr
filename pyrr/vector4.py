46550553c12ff66700b4a1a6d4e9c5f63e76a583
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-03-13 18:05:38 +1100

    Add comments to vector and add index values.

--- pyrr/vector4.py
+++ pyrr/vector4.py
@@ -19,7 +19,27 @@ def create_from_vector3( vector ):
 def create_from_matrix44_translation( mat ):
     return mat[ 3, 0:4 ].copy()
 
+
+class index:
+    #: The index of the X value within the vector
+    x = 0
+
+    #: The index of the Y value within the vector
+    y = 1
+
+    #: The index of the Z value within the vector
+    z = 2
+
+    #: The index of the W value within the vector
+    w = 3
+
+
 class unit:
+    #: A vector of unit length in the X-axis. (1.0, 0.0, 0.0, 0.0)
     x = create_unit_length_x()
+
+    #: A vector of unit length in the Y-axis. (0.0, 1.0, 0.0, 0.0)
     y = create_unit_length_y()
+
+    #: A vector of unit length in the Z-axis. (0.0, 0.0, 1.0, 0.0)
     z = create_unit_length_z()
