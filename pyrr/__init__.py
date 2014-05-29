8e925882d095a48046868f6b775b2137268d7113
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-05-30 00:00:02 +1000

    Use __all__ instead of import hax

--- pyrr/__init__.py
+++ pyrr/__init__.py
@@ -5,23 +5,25 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # this is used by the setup.py script
 from .version import __version__
 
-from . import aabb
-from . import aambb
-from . import euler
-from . import geometric_tests
-from . import geometry
-from . import integer
-from . import line
-from . import matrix
-from . import matrix33
-from . import matrix44
-from . import plane
-from . import quaternion
-from . import ray
-from . import rectangle
-from . import sphere
-from . import trig
-from . import utils
-from . import vector
-from . import vector3
-from . import vector4
+__all__ = [
+    'aabb',
+    'aambb',
+    'euler',
+    'geometric_tests',
+    'geometry',
+    'integer',
+    'line',
+    'matrix',
+    'matrix33',
+    'matrix44',
+    'plane',
+    'quaternion',
+    'ray',
+    'rectangle',
+    'sphere',
+    'trig',
+    'utils',
+    'vector',
+    'vector3',
+    'vector4',
+]
