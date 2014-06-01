def2809e7c035c42e46d908ed38b8a30691b6d99
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-06-01 23:05:23 +1000

    Rename int_type to value and add binary example.

--- pyrr/integer.py
+++ pyrr/integer.py
@@ -2,28 +2,30 @@
 """Provide functions for the manipulation of integers.
 """
 
-def count_bits(int_type):
+def count_bits(value):
     """Counts the number of bits set to 1 in an integer.
 
     For example::
 
-        >>> pyrr.integer.count_bits( 8 )
-        1
-        >>> pyrr.integer.count_bits( 3 )
-        2
-        >>> pyrr.integer.count_bits( 0xf )
+        >>> count_bits(0b101111)
+        5
+        >>> count_bits(0xf)
         4
+        >>> count_bits(8)
+        1
+        >>> count_bits(3)
+        2
     
-    :param int int_type: An integer.
+    :param int value: An integer.
     :rtype: integer
     :return: The count of bits set to 1.
 
     .. seealso:: http://wiki.python.org/moin/BitManipulation
     """
     count = 0
-    while (int_type):
-        count += (int_type & 1)
-        int_type >>= 1
+    while (value):
+        count += (value & 1)
+        value >>= 1
     
     return count
 
