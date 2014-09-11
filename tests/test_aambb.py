4252d599cb47d5d8fcbb8e022e549090e632bcfb
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-11 18:16:18 +1000

    Add tests for all functions and fix some tests

--- tests/test_aambb.py
+++ tests/test_aambb.py
@@ -1,140 +1,65 @@
 import unittest
-import math
-
-import numpy
-
+import numpy as np
 from pyrr import aambb
+from pyrr import aabb
+from pyrr import vector
 
 
-class test_aambb( unittest.TestCase ):
+class test_aambb(unittest.TestCase):
+    def test_create_zeros(self):
+        result = aambb.create_zeros()
+        self.assertTrue(np.array_equal(result, [[0.,0.,0.],[0.,0.,0.]]))
+        self.assertTrue(np.array_equal(aambb.centre_point(result), [0.0,0.0,0.0]))
 
-    def setUp( self ):
-        pass
+    def test_create_from_bounds(self):
+        bounds = [[-1.,1.,-1.], [2.,1.,0.]]
+        result = aambb.create_from_bounds(*bounds)
+        length = max(vector.length(bounds[0]), vector.length(bounds[1]))
+        self.assertTrue(np.array_equal(result, [[-length,-length,-length],[length,length,length]]))
+        self.assertTrue(np.array_equal(aambb.centre_point(result), [0.0,0.0,0.0]))
 
-    def tearDown( self ):
-        pass
+    def test_create_from_points(self):
+        result = aambb.create_from_points(np.array([[-1.0, 0.0, 0.0]]))
+        self.assertTrue(np.array_equal(result, [[-1.0,-1.0,-1.0],[ 1.0, 1.0, 1.0]]))
+        self.assertTrue(np.array_equal(aambb.centre_point(result), [0.0,0.0,0.0]))
 
-    def test_create_from_points( self ):
-        obj = aambb.create_from_points(
-            numpy.array(
-                [[-1.0, 0.0, 0.0]],
-                dtype = numpy.float
-                )
-            )
-        self.assertTrue(
-            numpy.array_equal( obj[ 0 ], [-1.0,-1.0,-1.0 ] ),
-            "Create from points failed"
-            )
-        self.assertTrue(
-            numpy.array_equal( obj[ 1 ], [ 1.0, 1.0, 1.0 ] ),
-            "Create from points failed"
-            )
-        self.assertTrue(
-            numpy.array_equal( aambb.centre_point(obj), [0.0,0.0,0.0 ] ),
-            "Create from points failed"
-            )
+    def test_center_point(self):
+        # this should always be 0,0,0
+        result = aambb.create_from_bounds([-1.,1.,-1.], [2.,1.,0.])
+        self.assertTrue(np.array_equal(aambb.centre_point(result), [0.0,0.0,0.0]))
 
-    def test_create_from_aabbs( self ):
-        return
+    def test_create_from_aabbs(self):
+        a1 = aambb.create_from_points([
+            [ 0.0, 0.0, 0.0],
+            [ 1.0, 1.0,-1.0]
+        ])
+        a2 = aambb.create_from_points([
+            [ 0.0, 0.0, 2.0],
+            [-1.0,-1.0, 1.0]
+        ])
+        result = aambb.create_from_aabbs([a1, a2])
+        length = np.amax(vector.length([a1, a2]))
+        self.assertTrue(np.array_equal(result, [[-length,-length,-length],[length,length,length]]), (result,))
+        self.assertTrue(np.array_equal(aambb.centre_point(result), [0.0,0.0,0.0]))
 
-        a1 = aabb.create_from_points(
-            [
-                [ 0.0, 0.0, 0.0 ],
-                [ 1.0, 1.0,-1.0 ]
-                ]
-            )
-        a2 = aabb.create_from_points(
-            [
-                [ 0.0, 0.0, 0.0 ],
-                [-1.0,-1.0, 1.0 ]
-                ]
-            )
-        a3 = aabb.create_from_aabbs(
-            numpy.array(
-                [ a1, a2 ]
-                )
-            )
+    def test_add_point(self):
+        a = aambb.create_from_bounds([-0.5,-0.5,-0.5], [0.5,0.5,0.5])
+        points = np.array([
+            [ 2.0,-1.0,-1.0],
+            [ 1.0, 3.0,-1.0],
+        ])
+        result = aambb.add_points(a, points)
+        length = np.amax(vector.length([a, points]))
+        self.assertTrue(np.array_equal(result, [[-length,-length,-length],[length,length,length]]), (result,))
+        self.assertTrue(np.array_equal(aambb.centre_point(result), [0.0,0.0,0.0]))
 
-        self.assertTrue(
-            numpy.array_equal(
-                a3[ 0 ],
-                [-1.0,-1.0,-1.0 ]
-                ),
-            "Create from AABBS failed"
-            )
-        self.assertTrue(
-            numpy.array_equal(
-                a3[ 1 ],
-                [ 1.0, 1.0, 1.0 ]
-                ),
-            "Create from AABBS failed"
-            )
-
-
-    def test_add_point( self ):
-        return
-
-        obj = numpy.array(
-            [
-                [-1.0,-1.0,-1.0],
-                [-1.0,-1.0,-1.0]
-                ],
-            dtype = numpy.float
-            )
-
-        aabb.add_points(
-            obj,
-            numpy.array(
-                [[ 1.0,-1.0,-1.0]],
-                dtype = numpy.float
-                ),
-            out = obj
-            )
-        self.assertTrue(
-            numpy.array_equal( obj[ 0 ], [-1.0,-1.0,-1.0] ),
-            "Add point failed"
-            )
-        self.assertTrue(
-            numpy.array_equal( obj[ 1 ], [ 1.0,-1.0,-1.0]  ),
-            "Add point failed"
-            )
-        self.assertTrue(
-            numpy.array_equal( aabb.centre_point( obj ), [ 0.0,-1.0,-1.0] ),
-            "Add point failed"
-            )
-
-    def test_add_aabbs( self ):
-        return
-
-        obj = aabb.create_from_points(
-            numpy.array(
-                [
-                    [-1.0,-1.0,-1.0],
-                    [ 1.0,-1.0,-1.0]
-                    ],
-                dtype = numpy.float
-                )
-            )
-
-        obj2 = aabb.create_from_bounds( [1.0,-2.0, 1.0], [2.0,-1.0, 1.0] )
-        aabb.add_aabbs(
-            obj,
-            numpy.array( [ obj2 ] ),
-            out = obj
-            )
-
-        self.assertTrue(
-            numpy.array_equal( aabb.minimum(obj), [-1.0,-2.0,-1.0] ),
-            "Add AABB failed"
-            )
-        self.assertTrue(
-            numpy.array_equal( aabb.maximum(obj), [ 2.0,-1.0, 1.0]  ),
-            "Add AABB failed"
-            )
-        self.assertTrue(
-            numpy.array_equal( aabb.centre_point(obj), [ 0.5,-1.5, 0.0] ),
-            "Add AABB failed"
-            )
+    def test_add_aabbs(self):
+        a1 = aambb.create_from_bounds([-0.5,-0.5,-0.5], [0.5,0.5,0.5])
+        a2 = aambb.create_from_bounds([1.0,-2.0, 1.0], [2.0,-1.0, 1.0])
+        result = aambb.add_aabbs(a1, [a2])
+        length = np.amax(vector.length([a1, a2]))
+        self.assertTrue(np.array_equal(result, [[-length,-length,-length],[length,length,length]]), (result,))
+        self.assertTrue(np.array_equal(aambb.centre_point(result), [0.0,0.0,0.0]))
 
 
 if __name__ == '__main__':
