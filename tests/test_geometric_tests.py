08b04a5d84d2b4b26e124d6d92569bca6ed0dae6
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-11 19:20:11 +1000

    Add tests for all functions, cleanup + fixes

--- tests/test_geometric_tests.py
+++ tests/test_geometric_tests.py
@@ -1,185 +1,250 @@
 import unittest
-import math
-
-import numpy
-
+import numpy as np
 from pyrr import geometric_tests as gt
+from pyrr import line, plane, ray, sphere
 
-from pyrr import line
-from pyrr import plane
-from pyrr import ray
-from pyrr import aabb
+# TODO: add way more tests
 
+class test_geometric_tests(unittest.TestCase):
+    def test_point_intersect_line(self):
+        p = np.array([1.,1.,1.])
+        l = np.array([[0.,0.,0.],[2.,2.,2.]])
+        result = gt.point_intersect_line(p, l)
+        self.assertTrue(np.array_equal(result, p))
 
-class test_geometric_tests( unittest.TestCase ):
+    def test_point_intersect_line_invalid(self):
+        p = np.array([3.,3.,3.])
+        l = np.array([[0.,0.,0.],[2.,2.,2.]])
+        result = gt.point_intersect_line(p, l)
+        self.assertTrue(np.array_equal(result, p))
 
-    def setUp( self ):
-        pass
+    def test_point_intersect_line_segment(self):
+        p = np.array([1.,1.,1.])
+        l = np.array([[0.,0.,0.],[2.,2.,2.]])
+        result = gt.point_intersect_line_segment(p, l)
+        self.assertTrue(np.array_equal(result, p))
 
-    def tearDown( self ):
-        pass
+    def test_point_intersect_line_segment_invalid(self):
+        p = np.array([3.,3.,3.])
+        l = np.array([[0.,0.,0.],[2.,2.,2.]])
+        result = gt.point_intersect_line_segment(p, l)
+        self.assertEqual(result, None)
 
-    def closest_point_on_line( self ):
-        new_line = line.create_from_points(
+    def test_point_intersect_rectangle_valid_intersections_1(self):
+        r = np.array([
+            [0.0, 0.0],
+            [5.0, 5.0]
+        ])
+        p = [ 0.0, 0.0]
+        result = gt.point_intersect_rectangle(p, r)
+        self.assertTrue(np.array_equal(result, p))
+
+    def test_point_intersect_rectangle_valid_intersections_2(self):
+        r = np.array([
+            [0.0, 0.0],
+            [5.0, 5.0]
+        ])
+        p = [ 5.0, 5.0]
+        result = gt.point_intersect_rectangle(p, r)
+        self.assertTrue(np.array_equal(result, p))
+
+    def test_point_intersect_rectangle_valid_intersections_3(self):
+        r = np.array([
+            [0.0, 0.0],
+            [5.0, 5.0]
+        ])
+        p = [ 1.0, 1.0]
+        result = gt.point_intersect_rectangle(p, r)
+        self.assertTrue(np.array_equal(result, p))
+
+    def test_point_intersect_rectangle_invalid_intersections_1(self):
+        r = np.array([
+            [0.0, 0.0],
+            [5.0, 5.0]
+        ])
+
+        p = [-1.0, 1.0]
+        result = gt.point_intersect_rectangle(p, r)
+        self.assertFalse(np.array_equal(result, p))
+
+    def test_point_intersect_rectangle_invalid_intersections_2(self):
+        r = np.array([
+            [0.0, 0.0],
+            [5.0, 5.0]
+        ])
+        p = [ 1.0, 10.0]
+        result = gt.point_intersect_rectangle(p, r)
+        self.assertFalse(np.array_equal(result, p))
+
+    def test_point_intersect_rectangle_invalid_intersections_3(self):
+        rect = np.array([
+            [0.0, 0.0],
+            [5.0, 5.0]
+        ])
+        point = [ 1.0,-1.0]
+        result = gt.point_intersect_rectangle(point, rect)
+        self.assertFalse(np.array_equal(result, point))
+
+    def test_ray_intersect_plane(self):
+        r = ray.create([0.,-1.,0.],[0.,1.,0.])
+        p = plane.create([0.,1.,0.], 0.)
+        result = gt.ray_intersect_plane(r, p)
+        self.assertFalse(np.array_equal(result, [0.,1.,0.]))
+
+    def test_ray_intersect_plane_front_only(self):
+        r = ray.create([0.,-1.,0.],[0.,1.,0.])
+        p = plane.create([0.,1.,0.], 0.)
+        result = gt.ray_intersect_plane(r, p, front_only=True)
+        self.assertEqual(result, None)
+
+    def test_ray_intersect_plane_invalid(self):
+        r = ray.create([0.,-1.,0.],[1.,0.,0.])
+        p = plane.create([0.,1.,0.], 0.)
+        result = gt.ray_intersect_plane(r, p)
+        self.assertEqual(result, None)
+
+    def test_point_closest_point_on_ray(self):
+        l = line.create_from_points(
             [ 0.0, 0.0, 0.0 ],
             [10.0, 0.0, 0.0 ]
-            )
-        points = numpy.array( [ 0.5, 1.0, 0.0 ] )
+        )
+        p = np.array([ 0.0, 1.0, 0.0])
+        result = gt.point_closest_point_on_ray(p, l)
+        self.assertTrue(np.array_equal(result, [ 0.0, 0.0, 0.0]))
 
-        result = gt.closest_point_on_line( new_line, point )
+    def test_point_closest_point_on_line(self):
+        p = np.array([0.,1.,0.])
+        l = np.array([[0.,0.,0.],[2.,0.,0.]])
+        result = gt.point_closest_point_on_line(p, l)
+        self.assertTrue(np.array_equal(result, [0.,0.,0.]), (result,))
 
-        expected = numpy.array( [ 0.5, 0.0, 0.0 ] )
+    def test_point_closest_point_on_line_2(self):
+        p = np.array([3.,0.,0.])
+        l = np.array([[0.,0.,0.],[2.,0.,0.]])
+        result = gt.point_closest_point_on_line(p, l)
+        self.assertTrue(np.array_equal(result, [3.,0.,0.]), (result,))
 
-        self.assertTrue(
-            numpy.array_equal( result, expected ),
-            "Closest point on plane incorrect"
-            )
+    def test_point_closest_point_on_line_segment(self):
+        p = np.array([0.,1.,0.])
+        l = np.array([[0.,0.,0.],[2.,0.,0.]])
+        result = gt.point_closest_point_on_line_segment(p, l)
+        self.assertTrue(np.array_equal(result, [0.,0.,0.]), (result,))
 
-    def test_height_above_plane( self ):
-        v1 = numpy.array( [ 0.0, 0.0, 1.0 ] )
-        v2 = numpy.array( [ 1.0, 0.0, 1.0 ] )
-        v3 = numpy.array( [ 0.0, 1.0, 1.0 ] )
-        point = numpy.array([ 0.0, 0.0, 20.0 ])
+    def test_vector_parallel_vector(self):
+        v1 = np.array([1.,0.,0.])
+        v2 = np.array([2.,0.,0.])
+        self.assertTrue(gt.vector_parallel_vector(v1,v2))
 
-        p = plane.create_from_points( v1, v2, v3 )
-        p = plane.invert_normal( p )
+    def test_vector_parallel_vector_invalid(self):
+        v1 = np.array([1.,0.,0.])
+        v2 = np.array([0.,1.,0.])
+        self.assertTrue(False == gt.vector_parallel_vector(v1,v2))
 
-        result = gt.point_height_above_plane( point, p )
+    def test_ray_parallel_ray(self):
+        r1 = ray.create([0.,0.,0.],[1.,0.,0.])
+        r2 = ray.create([1.,0.,0.],[2.,0.,0.])
+        self.assertTrue(gt.ray_parallel_ray(r1,r2))
 
-        # should be 19.0
-        expected = 19.0
+    def test_ray_parallel_ray_2(self):
+        r1 = ray.create([0.,0.,0.],[1.,0.,0.])
+        r2 = ray.create([1.,0.,0.],[0.,1.,0.])
+        self.assertTrue(False == gt.ray_parallel_ray(r1,r2))
 
-        self.assertEqual(
-            result,
-            expected,
-            "Height above plane incorrect"
-            )
+    def test_ray_parallel_ray_3(self):
+        r1 = ray.create([0.,0.,0.],[1.,0.,0.])
+        r2 = ray.create([0.,1.,0.],[1.,0.,0.])
+        self.assertTrue(gt.ray_parallel_ray(r1,r2))
 
-    def test_closest_point_on_plane( self ):
-        p = numpy.array( [ 0.0, 1.0, 0.0, 0.0 ] )
-        point = numpy.array([ 5.0, 20.0, 5.0 ])
-        
-        result = gt.point_closest_point_on_plane( point, p )
+    def test_ray_coincident_ray(self):
+        r1 = ray.create([0.,0.,0.],[1.,0.,0.])
+        r2 = ray.create([1.,0.,0.],[2.,0.,0.])
+        self.assertTrue(gt.ray_coincident_ray(r1,r2))
 
-        # should be # 0, 0, 1
-        expected = numpy.array( [ 5.0, 0.0, 5.0 ] )
+    def test_ray_coincident_ray_2(self):
+        r1 = ray.create([0.,0.,0.],[1.,0.,0.])
+        r2 = ray.create([1.,0.,0.],[0.,1.,0.])
+        self.assertTrue(False == gt.ray_coincident_ray(r1,r2))
 
-        self.assertTrue(
-            numpy.array_equal( result, expected ),
-            "Closest point on plane incorrect"
-            )
+    def test_ray_coincident_ray_3(self):
+        r1 = ray.create([0.,0.,0.],[1.,0.,0.])
+        r2 = ray.create([0.,1.,0.],[1.,0.,0.])
+        self.assertTrue(False == gt.ray_coincident_ray(r1,r2))
 
-    def test_point_intersect_rectangle( self ):
-        def valid_intersections():
-            rect = numpy.array(
-                [
-                    [0.0, 0.0],
-                    [5.0, 5.0]
-                    ]
-                )
+    def test_ray_intersect_aabb_valid_1(self):
+        a = np.array([[-1.0,-1.0,-1.0], [ 1.0, 1.0, 1.0]])
+        r = np.array([[ 0.5, 0.5, 0.0], [ 0.0, 0.0,-1.0]])
+        result = gt.ray_intersect_aabb(r, a)
+        self.assertTrue(np.array_equal(result, [ 0.5, 0.5,-1.0]))
 
-            def point_1():
-                point = [ 0.0, 0.0 ]
-                result = gt.point_intersect_rectangle( point, rect )
+    def test_ray_intersect_aabb_valid_2(self):
+        a = np.array([[-1.0,-1.0,-1.0], [ 1.0, 1.0, 1.0]])
+        r = np.array([[2.0, 2.0, 2.0], [ -1.0, -1.0, -1.0]])
+        result = gt.ray_intersect_aabb(r, a)
+        self.assertTrue(np.array_equal(result, [1.0, 1.0, 1.0]))
 
-                self.assertTrue(
-                    numpy.array_equal( result, point )
-                    )
-            point_1()
+    def test_ray_intersect_aabb_invalid_1(self):
+        a = np.array([[-1.0,-1.0,-1.0], [ 1.0, 1.0, 1.0]])
+        r = np.array([[2.0, 2.0, 2.0], [ 1.0, 1.0, 1.0]])
+        result = gt.ray_intersect_aabb(r, a)
+        self.assertEqual(result, None)
 
-            def point_2():
-                point = [ 5.0, 5.0 ]
-                result = gt.point_intersect_rectangle( point, rect )
+    def test_point_height_above_plane(self):
+        v1 = np.array([ 0.0, 0.0, 1.0])
+        v2 = np.array([ 1.0, 0.0, 1.0])
+        v3 = np.array([ 0.0, 1.0, 1.0])
+        p = np.array([0.0, 0.0, 20.0])
 
-                self.assertTrue(
-                    numpy.array_equal( result, point )
-                    )
-            point_2()
+        pl = plane.create_from_points(v1, v2, v3)
+        pl = plane.invert_normal(pl)
 
-            def point_3():
-                point = [ 1.0, 1.0 ]
-                result = gt.point_intersect_rectangle( point, rect )
+        result = gt.point_height_above_plane(p, pl)
+        self.assertEqual(result, 19.)
 
-                self.assertTrue(
-                    numpy.array_equal( result, point )
-                    )
-            point_3()
-        valid_intersections()
+    def test_point_closest_point_on_plane(self):
+        pl = np.array([ 0.0, 1.0, 0.0, 0.0])
+        p = np.array([ 5.0, 20.0, 5.0])
+        result = gt.point_closest_point_on_plane(p, pl)
+        self.assertTrue(np.array_equal(result, [ 5.0, 0.0, 5.0]))
 
-        def invalid_intersections():
-            rect = numpy.array(
-                [
-                    [0.0, 0.0],
-                    [5.0, 5.0]
-                    ]
-                )
+    def test_sphere_does_intersect_sphere_1(self):
+        s1 = sphere.create()
+        s2 = sphere.create()
+        self.assertTrue(gt.sphere_does_intersect_sphere(s1, s2))
 
-            def point_1():
-                point = [-1.0, 1.0 ]
-                result = gt.point_intersect_rectangle( point, rect )
+    def test_sphere_does_intersect_sphere_2(self):
+        s1 = sphere.create()
+        s2 = sphere.create([1.,0.,0.])
+        self.assertTrue(gt.sphere_does_intersect_sphere(s1, s2))
 
-                self.assertFalse(
-                    numpy.array_equal( result, point )
-                    )
-            point_1()
+    def test_sphere_does_intersect_sphere_3(self):
+        s1 = sphere.create()
+        s2 = sphere.create([2.,0.,0.], 1.0)
+        self.assertTrue(gt.sphere_does_intersect_sphere(s1, s2))
 
-            def point_2():
-                point = [ 1.0, 10.0 ]
-                result = gt.point_intersect_rectangle( point, rect )
+    def test_sphere_does_intersect_sphere_4(self):
+        s1 = sphere.create()
+        s2 = sphere.create([2.,0.,0.], 0.5)
+        self.assertTrue(False == gt.sphere_does_intersect_sphere(s1, s2))
 
-                self.assertFalse(
-                    numpy.array_equal( result, point )
-                    )
-            point_2()
+    def test_sphere_penetration_sphere_1(self):
+        s1 = sphere.create()
+        s2 = sphere.create()
+        self.assertEqual(gt.sphere_penetration_sphere(s1, s2), 2.0)
 
-            def point_3():
-                point = [ 1.0,-1.0 ]
-                result = gt.point_intersect_rectangle( point, rect )
+    def test_sphere_penetration_sphere_2(self):
+        s1 = sphere.create()
+        s2 = sphere.create([1.,0.,0.], 1.0)
+        self.assertEqual(gt.sphere_penetration_sphere(s1, s2), 1.0)
 
-                self.assertFalse(
-                    numpy.array_equal( result, point )
-                    )
-            point_3()
-        invalid_intersections()
+    def test_sphere_penetration_sphere_3(self):
+        s1 = sphere.create()
+        s2 = sphere.create([2.,0.,0.], 1.0)
+        self.assertEqual(gt.sphere_penetration_sphere(s1, s2), 0.0)
 
-    def test_ray_intersect_aabb( self ):
-        def valid_intersections():
-            def ray_1():
-                aabb = numpy.array( [ [-1.0,-1.0,-1.0 ], [ 1.0, 1.0, 1.0 ] ] )
-                ray = numpy.array( [ [ 0.5, 0.5, 0.0 ], [ 0.0, 0.0,-1.0 ] ] )
-                expected = numpy.array( [ 0.5, 0.5,-1.0 ] )
-
-                result = gt.ray_intersect_aabb( ray, aabb )
-
-                self.assertTrue(
-                    numpy.array_equal( result, expected ),
-                    "Ray vs AABB intersection incorrect: expected %s, got %s" % (
-                        expected, result
-                        )
-                    )
-            ray_1()
-
-            def ray_2():
-                aabb = numpy.array( [ [-1.0,-1.0,-1.0 ], [ 1.0, 1.0, 1.0 ] ] )
-                ray = numpy.array( [ [2.0, 2.0, 2.0 ], [ -1.0, -1.0, -1.0 ] ] )
-                expected = numpy.array( [1.0, 1.0, 1.0] )
-
-                result = gt.ray_intersect_aabb( ray, aabb )
-
-                self.assertTrue(
-                    numpy.array_equal( result, expected ),
-                    "Ray vs AABB intersection incorrect: expected %s, got %s" % (
-                        expected, result
-                        )
-                    )
-            ray_2()
-        valid_intersections()
-
-        def invalid_intersections():
-            pass
-        invalid_intersections()
-
-
+    def test_sphere_penetration_sphere_4(self):
+        s1 = sphere.create()
+        s2 = sphere.create([3.,0.,0.], 1.0)
+        self.assertEqual(gt.sphere_penetration_sphere(s1, s2), 0.0)
 
 if __name__ == '__main__':
     unittest.main()
