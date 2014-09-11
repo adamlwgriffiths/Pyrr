8a41f74855e3f44182beac35b9d2f82ec54768c2
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-11 20:19:17 +1000

    Add tests for trig

---
+++ tests/test_trig.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+from pyrr import trig
+
+
+class test_trig(unittest.TestCase):
+    def test_aspec_ratio(self):
+        self.assertEqual(trig.aspect_ratio(1920, 1080), 1920./1080.)
+
+    @unittest.skip('Need a test here')
+    def test_calculate_fov(self):
+        pass
+
+    @unittest.skip('Need a test here')
+    def test_calculate_zoom(self):
+        pass
+
+    @unittest.skip('Need a test here')
+    def test_calculate_height(self):
+        pass
+
+    @unittest.skip('Need a test here')
+    def test_calculate_plane_size(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
