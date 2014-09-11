6b2521699dca3b8331cbadc2f4cf4e26f744f046
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-11 18:15:55 +1000

    Add tests for all functions and fix some tests

--- tests/test_aabb.py
+++ tests/test_aabb.py
@@ -1,150 +1,146 @@
 import unittest
-import math
-
-import numpy
-
+import numpy as np
 from pyrr import aabb
 
 
-class test_aabb( unittest.TestCase ):
+class test_aabb(unittest.TestCase):
+    def test_create_zeros(self):
+        result = aabb.create_zeros()
+        self.assertTrue(np.array_equal(result, [[0,0,0],[0,0,0]]))
 
-    def setUp( self ):
-        pass
+    def test_create_from_bounds(self):
+        result = aabb.create_from_bounds([-1,-1,-1],[1,1,1])
+        self.assertTrue(np.array_equal(result, [[-1,-1,-1],[1,1,1]]))
 
-    def tearDown( self ):
-        pass
-
-    def test_create_from_points( self ):
-        """
-        points = numpy.array(
-            [
+    def test_create_from_points(self):
+        points = np.array([[-1.0,-1.0,-1.0]])
+        result = aabb.create_from_points(points)
+        expected = np.array([
+                [-1.0,-1.0,-1.0],
                 [-1.0,-1.0,-1.0]
-                ]
-            )
-        """
-        points = numpy.array( [ [-1.0,-1.0,-1.0 ] ] )
-        result = aabb.create_from_points( points )
-        
-        expected = numpy.tile( points, (2,1) )
+        ])
+        self.assertTrue(np.array_equal(result, expected))
 
-        self.assertTrue(
-            numpy.array_equal( result, expected ),
-            "Create from points failed"
-            )
+        points = np.array([
+            [-1.0,-1.0,-1.0],
+            [-1.0, 2.0,-2.0],
+        ])
+        result = aabb.create_from_points(points)
+        expected = np.array([
+            [-1.0,-1.0,-2.0],
+            [-1.0, 2.0,-1.0],
+        ])
+        self.assertTrue(np.array_equal(result, expected))
 
-    def test_centre_point( self ):
-        def single_point():
-            points = numpy.array( [ [-1.0,-1.0,-1.0 ] ] )
-            obj = aabb.create_from_points( points )
-            result = aabb.centre_point( obj )
-
-            expected = numpy.array( [-1.0,-1.0,-1.0 ] )
-
-            self.assertTrue(
-                numpy.array_equal( result, expected ),
-                "AABB single point centre point incorrect"
-                )
-        single_point()
-
-        def multiple_points():
-            points = numpy.array(
-                [
-                    [ 1.0, 1.0, 1.0 ],
-                    [-1.0,-1.0,-1.0 ]
-                    ]
-                )
-            obj = aabb.create_from_points( points )
-            result = aabb.centre_point( obj )
-
-            expected = numpy.zeros( 3 )
-
-            self.assertTrue(
-                numpy.array_equal( result, expected ),
-                "AABB multiple points centre point incorrect"
-                )
-        multiple_points()
-
-    def test_create_from_aabbs( self ):
+    def test_create_from_aabbs(self):
         # -1
-        a1 = numpy.array(
-            [
-                [-1.0,-1.0,-1.0 ],
-                [-1.0,-1.0,-1.0 ]
-                ]
-            )
+        a1 = np.array([
+            [-1.0, 0.0, 0.0 ],
+            [-1.0,-1.0,-1.0 ]
+        ])
         # +1
-        a2 = numpy.array(
-            [
-                [ 1.0, 1.0, 1.0 ],
-                [ 1.0, 1.0, 1.0 ]
-                ]
-            )
+        a2 = np.array([
+            [ 1.0,-1.0,-1.0 ],
+            [ 1.0, 1.0, 1.0 ]
+        ])
 
         # -1 to +1
-        result = aabb.create_from_aabbs( numpy.array( [ a1, a2 ] ) )
+        result = aabb.create_from_aabbs(np.array([a1, a2]))
+        expected = np.array([
+            [-1.0,-1.0,-1.0 ],
+            [ 1.0, 1.0, 1.0 ]
+        ])
 
-        expected = numpy.array(
-            [
-                [-1.0,-1.0,-1.0 ],
-                [ 1.0, 1.0, 1.0 ]
-                ]
-            )
+        self.assertTrue(np.array_equal(result, expected))
 
-        self.assertTrue(
-            numpy.array_equal( result, expected ),
-            "Create from AABBS failed"
-            )
+    def test_add_point(self):
+        obj = np.array([
+            [-1.0,-1.0,-1.0],
+            [-1.0,-1.0,-1.0]
+        ])
+        points = np.array([1.0, 1.0, 1.0])
+        result = aabb.add_points(obj, points)
+        expected = np.array([
+            [-1.0,-1.0,-1.0 ],
+            [ 1.0, 1.0, 1.0 ]
+        ])
+        self.assertTrue(np.array_equal(result, expected))
 
-    def test_add_point( self ):
-        obj = numpy.array(
-            [
-                [-1.0,-1.0,-1.0],
-                [-1.0,-1.0,-1.0]
-                ]
-            )
-        points = numpy.array( [ 1.0, 1.0, 1.0] )
+    def test_add_aabbs(self):
+        a = aabb.create_zeros()
 
-        result = aabb.add_points( obj, points )
+        a1 = np.array([
+            [-1.0,-1.0,-1.0],
+            [ 1.0, 1.0, 1.0]
+        ])
+        result = aabb.add_aabbs(a, a1)
+        expected = np.array([
+            [-1.0,-1.0,-1.0],
+            [ 1.0, 1.0, 1.0]
+        ])
+        self.assertTrue(np.array_equal(result, expected))
 
-        expected = numpy.array(
-            [
-                [-1.0,-1.0,-1.0 ],
-                [ 1.0, 1.0, 1.0 ]
-                ]
-            )
 
-        self.assertTrue(
-            numpy.array_equal( result, expected),
-            "Add point failed"
-            )
+        a = np.array([
+            [-1.0,-1.0,-1.0],
+            [ 1.0, 1.0, 1.0]
+        ])
+        a2 = np.array([
+            [-1.0, 0.0,-1.0],
+            [ 2.0, 1.0, 1.0]
+        ])
+        result = aabb.add_aabbs(a, a2)
+        expected = np.array([
+            [-1.0,-1.0,-1.0],
+            [ 2.0, 1.0, 1.0]
+        ])
+        self.assertTrue(np.array_equal(result, expected))
 
-    def test_add_aabbs( self ):
-        a1 = numpy.array(
-            [
-                [-1.0,-1.0,-1.0],
-                [-1.0,-1.0,-1.0]
-                ]
-            )
-        a2 = numpy.array(
-            [
-                [ 1.0, 1.0, 1.0],
-                [ 1.0, 1.0, 1.0]
-                ]
-            )
-        result = aabb.add_aabbs( a1, a2 )
+    def test_centre_point_single_point(self):
+        points = np.array([[-1.0,-1.0,-1.0]])
+        obj = aabb.create_from_points(points)
+        result = aabb.centre_point(obj)
+        expected = np.array([-1.0,-1.0,-1.0])
+        self.assertTrue(np.array_equal(result, expected))
 
-        expected = numpy.array(
-            [
-                [-1.0,-1.0,-1.0 ],
-                [ 1.0, 1.0, 1.0 ]
-                ]
-            )
+    def test_centre_point_multiple_points(self):
+        points = np.array([
+            [ 1.0, 1.0, 1.0],
+            [-1.0,-1.0,-1.0]
+        ])
+        obj = aabb.create_from_points(points)
+        result = aabb.centre_point(obj)
+        expected = np.zeros(3)
+        self.assertTrue(np.array_equal(result, expected))
 
-        self.assertTrue(
-            numpy.array_equal( result, expected ),
-            "Add AABB failed"
-            )
+    def test_minimum(self):
+        a = aabb.create_from_bounds([-1,-1,-1],[1,1,1])
+        result = aabb.minimum(a)
+        self.assertTrue(np.array_equal(result, [-1,-1,-1]))
 
+    def test_maximum(self):
+        a = aabb.create_from_bounds([-1,-1,-1],[1,1,1])
+        result = aabb.maximum(a)
+        self.assertTrue(np.array_equal(result, [1,1,1]))
+
+    def test_clamp_points_single(self):
+        a = aabb.create_from_bounds([-1,-1,-1],[1,1,1])
+        points = np.array([2,1,1])
+        result = aabb.clamp_points(a, points)
+        expected = np.array([1,1,1])
+        self.assertTrue(np.array_equal(result, expected))
+
+    def test_clamp_points_list(self):
+        a = aabb.create_from_bounds([-1,-1,-1],[1,1,1])
+        points = np.array([
+            [1,1,1],
+            [2,1,1],
+            [-1,-1,-1],
+            [-2,-2,-2],
+        ])
+        result = aabb.clamp_points(a, points)
+        expected = np.array([[1,1,1],[1,1,1],[-1,-1,-1],[-1,-1,-1]])
+        self.assertTrue(np.array_equal(result, expected))
 
 if __name__ == '__main__':
     unittest.main()
