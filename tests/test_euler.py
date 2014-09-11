cecc608bddee4d9864011cf9db076e41dca62970
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-11 18:19:07 +1000

    Add tests for euler

---
+++ tests/test_euler.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+from pyrr import euler
+
+class test_euler(unittest.TestCase):
+    def test_create(self):
+        self.assertTrue(np.array_equal(euler.create(), [0.,0.,0.]))
+        self.assertTrue(np.array_equal(euler.create(1.,2.,3.), [1.,2.,3.]))
+
+    def test_pitch(self):
+        self.assertEqual(euler.pitch([1.,2.,3.]), 1.)
+
+    def test_roll(self):
+        self.assertEqual(euler.roll([1.,2.,3.]), 2.)
+
+    def test_yaw(self):
+        self.assertEqual(euler.yaw([1.,2.,3.]), 3.)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
