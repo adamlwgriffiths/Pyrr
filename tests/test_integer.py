ba0c213bfbef7c88b430bc7b3b02df5a2f60799e
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-11 19:48:12 +1000

    Add tests for integer

---
+++ tests/test_integer.py
@@ -0,0 +1,11 @@
+import unittest
+import numpy as np
+from pyrr import integer
+
+class test_integer(unittest.TestCase):
+    def test_count_bits(self):
+        i = 0b010101
+        self.assertEqual(integer.count_bits(i), 3)
+
+if __name__ == '__main__':
+    unittest.main()
