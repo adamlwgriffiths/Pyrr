dafbab1f2a6504ecc0fad34af470998bd7c0b63c
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-11 20:19:02 +1000

    Simplify test

--- tests/test_matrix_quaternion.py
+++ tests/test_matrix_quaternion.py
@@ -9,18 +9,10 @@ class test_matrix_quaternion(unittest.TestCase):
         Create a matrix and quaternion, rotate each by the same values
         then convert matrix<->quaternion and check the results are the same.
         """
-        m1 = matrix44.create_identity()
-        m2 = matrix44.create_from_x_rotation(np.pi)
-        m = matrix44.multiply(m1, m2)
-
-        q1 = quaternion.create()
-        q2 = quaternion.create_from_x_rotation(np.pi)
-        q = quaternion.cross(q1, q2)
-
+        m = matrix44.create_from_x_rotation(np.pi)
+        q = quaternion.create_from_x_rotation(np.pi)
         qm = matrix44.create_from_quaternion(q)
-
         mq = quaternion.create_from_matrix(m)
-
         np.testing.assert_almost_equal(q, mq, decimal=5)
         np.testing.assert_almost_equal(m, qm, decimal=5)
 
