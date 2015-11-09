Fix quaternion rotation, #34.

pyrr/quaternion.py
@@ -192,14 +192,10 @@ def cross(quat1, quat2):
 
     return np.array(
         [
-            # x = q1.w * q2.x + q1.x * q2.w + q1.z * q2.y - q1.y * q2.z 
-            (q1w * q2x) + (q1x * q2w) + (q1z * q2y) - (q1y * q2z),
-            # y = q1.w * q2.y + q1.y * q2.w + q1.x * q2.z - q1.z * q2.x
-            (q1w * q2y) + (q1y * q2w) + (q1x * q2z) - (q1z * q2x),
-            # z = q1.w * q2.z + q1.z * q2.w + q1.y * q2.x - q1.x * q2.y
-            (q1w * q2z) + (q1z * q2w) + (q1y * q2x) - (q1x * q2y),
-            # w = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z
-            (q1w * q2w) - (q1x * q2x) - (q1y * q2y) - (q1z * q2z)
+             q1x * q2w + q1y * q2z - q1z * q2y + q1w * q2x,
+            -q1x * q2z + q1y * q2w + q1z * q2x + q1w * q2y,
+             q1x * q2y - q1y * q2x + q1z * q2w + q1w * q2z,
+            -q1x * q2x - q1y * q2y - q1z * q2z + q1w * q2w,
         ],
         dtype=quat1.dtype
     )

tests/test_quaternion.py
@@ -8,6 +8,7 @@ from pyrr import quaternion
 
 class test_quaternion(unittest.TestCase):
     # many of these values are taken from searches on wolfram alpha
+
     def test_import(self):
         import pyrr
         pyrr.quaternion
@@ -15,7 +16,7 @@ class test_quaternion(unittest.TestCase):
 
     def test_create(self):
         result = quaternion.create()
-        np.testing.assert_almost_equal(result, [0.,0.,0.,1.], decimal=5)
+        np.testing.assert_almost_equal(result, [0., 0., 0., 1.], decimal=5)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_parameters(self):
@@ -25,60 +26,60 @@ class test_quaternion(unittest.TestCase):
 
     def test_create_from_x_rotation(self):
         result = quaternion.create_from_x_rotation(np.pi)
-        np.testing.assert_almost_equal(result, [1.,0.,0.,0.], decimal=3)
+        np.testing.assert_almost_equal(result, [1., 0., 0., 0.], decimal=3)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_y_rotation(self):
         result = quaternion.create_from_y_rotation(np.pi)
-        np.testing.assert_almost_equal(result, [0.,1.,0.,0.], decimal=3)
+        np.testing.assert_almost_equal(result, [0., 1., 0., 0.], decimal=3)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_z_rotation(self):
         result = quaternion.create_from_z_rotation(np.pi)
-        np.testing.assert_almost_equal(result, [0.,0.,1.,0.], decimal=3)
+        np.testing.assert_almost_equal(result, [0., 0., 1., 0.], decimal=3)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_axis_rotation(self):
         # wolfram alpha can be awesome sometimes
-        result = quaternion.create_from_axis_rotation([0.57735, 0.57735, 0.57735],np.pi)
+        result = quaternion.create_from_axis_rotation([0.57735, 0.57735, 0.57735], np.pi)
         np.testing.assert_almost_equal(result, [5.77350000e-01, 5.77350000e-01, 5.77350000e-01, 6.12323400e-17], decimal=3)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_axis_rotation_non_normalised(self):
-        result = quaternion.create_from_axis_rotation([1.,1.,1.], np.pi)
+        result = quaternion.create_from_axis_rotation([1., 1., 1.], np.pi)
         np.testing.assert_almost_equal(result, [5.77350000e-01, 5.77350000e-01, 5.77350000e-01, 6.12323400e-17], decimal=3)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_matrix_unit(self):
         result = quaternion.create_from_matrix(np.eye(3))
-        np.testing.assert_almost_equal(result, [0.,0.,0.,1.], decimal=5)
+        np.testing.assert_almost_equal(result, [0., 0., 0., 1.], decimal=5)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_matrix_x(self):
         result = quaternion.create_from_matrix([
-            [1.,0.,0.],
-            [0.,-1.,0.],
-            [0.,0.,-1.],
+            [1., 0., 0.],
+            [0., -1., 0.],
+            [0., 0., -1.],
         ])
-        np.testing.assert_almost_equal(result, [1.,0.,0.,0.], decimal=5)
+        np.testing.assert_almost_equal(result, [1., 0., 0., 0.], decimal=5)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_matrix_y(self):
         result = quaternion.create_from_matrix([
-            [-1.,0.,0.],
-            [0.,1.,0.],
-            [0.,0.,-1.],
+            [-1., 0., 0.],
+            [0., 1., 0.],
+            [0., 0., -1.],
         ])
-        np.testing.assert_almost_equal(result, [0.,1.,0.,0.], decimal=5)
+        np.testing.assert_almost_equal(result, [0., 1., 0., 0.], decimal=5)
         self.assertTrue(result.dtype == np.float)
 
     def test_create_from_matrix_z(self):
         result = quaternion.create_from_matrix([
-            [-1.,0.,0.],
-            [0.,-1.,0.],
-            [0.,0.,1.],
+            [-1., 0., 0.],
+            [0., -1., 0.],
+            [0., 0., 1.],
         ])
-        np.testing.assert_almost_equal(result, [0.,0.,1.,0.], decimal=5)
+        np.testing.assert_almost_equal(result, [0., 0., 1., 0.], decimal=5)
         self.assertTrue(result.dtype == np.float)
 
     @unittest.skip('Not implemented')
@@ -96,70 +97,70 @@ class test_quaternion(unittest.TestCase):
         np.testing.assert_almost_equal(result, quaternion.create(), decimal=5)
 
     def test_is_zero_length(self):
-        result = quaternion.is_zero_length([1.,0.,0.,0.])
+        result = quaternion.is_zero_length([1., 0., 0., 0.])
         self.assertFalse(result)
 
     def test_is_zero_length_zero(self):
-        result = quaternion.is_zero_length([0.,0.,0.,0.])
+        result = quaternion.is_zero_length([0., 0., 0., 0.])
         self.assertTrue(result)
 
     def test_is_non_zero_length(self):
-        result = quaternion.is_non_zero_length([1.,0.,0.,0.])
+        result = quaternion.is_non_zero_length([1., 0., 0., 0.])
         self.assertTrue(result)
 
     def test_is_non_zero_length_zero(self):
-        result = quaternion.is_non_zero_length([0.,0.,0.,0.])
+        result = quaternion.is_non_zero_length([0., 0., 0., 0.])
         self.assertFalse(result)
 
     def test_squared_length_identity(self):
-        result = quaternion.squared_length([0.,0.,0.,1.])
+        result = quaternion.squared_length([0., 0., 0., 1.])
         np.testing.assert_almost_equal(result, 1., decimal=5)
 
     def test_squared_length(self):
-        result = quaternion.squared_length([1.,1.,1.,1.])
+        result = quaternion.squared_length([1., 1., 1., 1.])
         np.testing.assert_almost_equal(result, 4., decimal=5)
 
     def test_squared_length_batch(self):
         result = quaternion.squared_length([
-            [0.,0.,0.,1.],
-            [1.,1.,1.,1.],
+            [0., 0., 0., 1.],
+            [1., 1., 1., 1.],
         ])
-        np.testing.assert_almost_equal(result, [1.,4.], decimal=5)
+        np.testing.assert_almost_equal(result, [1., 4.], decimal=5)
 
     def test_length_identity(self):
-        result = quaternion.length([0.,0.,0.,1.])
+        result = quaternion.length([0., 0., 0., 1.])
         np.testing.assert_almost_equal(result, 1., decimal=5)
 
     def test_length(self):
-        result = quaternion.length([1.,1.,1.,1.])
+        result = quaternion.length([1., 1., 1., 1.])
         np.testing.assert_almost_equal(result, 2., decimal=5)
 
     def test_length_batch(self):
         result = quaternion.length([
-            [0.,0.,0.,1.],
-            [1.,1.,1.,1.],
+            [0., 0., 0., 1.],
+            [1., 1., 1., 1.],
         ])
-        np.testing.assert_almost_equal(result, [1.,2.], decimal=5)
+        np.testing.assert_almost_equal(result, [1., 2.], decimal=5)
 
     def test_normalise_identity(self):
         # normalise an identity quaternion
-        result = quaternion.normalise([0.,0.,0.,1.])
-        np.testing.assert_almost_equal(result, [0.,0.,0.,1.], decimal=5)
+        result = quaternion.normalise([0., 0., 0., 1.])
+        np.testing.assert_almost_equal(result, [0., 0., 0., 1.], decimal=5)
 
     def test_normalise_non_identity(self):
         # normalise an identity quaternion
-        result = quaternion.normalise([1.,2.,3.,4.])
-        np.testing.assert_almost_equal(result, [1./np.sqrt(30.),np.sqrt(2./15.),np.sqrt(3./10.),2.*np.sqrt(2./15.)], decimal=5)
+        result = quaternion.normalise([1., 2., 3., 4.])
+        np.testing.assert_almost_equal(result, [1. / np.sqrt(30.), np.sqrt(2. / 15.), np.sqrt(3. / 10.), 2. * np.sqrt(2. / 15.)], decimal=5)
 
     def test_normalise_batch(self):
         # normalise an identity quaternion
         result = quaternion.normalise([
-            [0.,0.,0.,1.],
-            [1.,2.,3.,4.],
+            [0., 0., 0., 1.],
+            [1., 2., 3., 4.],
         ])
         expected = [
-            [0.,0.,0.,1.],
-            [1./np.sqrt(30.),np.sqrt(2./15.),np.sqrt(3./10.),2.*np.sqrt(2./15.)],
+            [0., 0., 0., 1.],
+            [1. / np.sqrt(30.), np.sqrt(2. / 15.), np.sqrt(3. / 10.), 2. * np.sqrt(2. / 15.)],
         ]
         np.testing.assert_almost_equal(result, expected, decimal=5)
 
@@ -172,34 +173,34 @@ class test_quaternion(unittest.TestCase):
         np.testing.assert_almost_equal(result, [0.57735, 0.57735, 0.57735], decimal=5)
 
     def test_dot_adjacent(self):
-        result = quaternion.dot([1.,0.,0.,0.], [0.,1.,0.,0.])
+        result = quaternion.dot([1., 0., 0., 0.], [0., 1., 0., 0.])
         np.testing.assert_almost_equal(result, 0.0, decimal=5)
 
     def test_dot_parallel(self):
-        result = quaternion.dot([0.,1.,0.,0.], [0.,1.,0.,0.])
+        result = quaternion.dot([0., 1., 0., 0.], [0., 1., 0., 0.])
         np.testing.assert_almost_equal(result, 1.0, decimal=5)
 
     def test_dot_angle(self):
-        result = quaternion.dot([.2,.2,0.,0.], [2.,-.2,0.,0.])
+        result = quaternion.dot([.2, .2, 0., 0.], [2., -.2, 0., 0.])
         np.testing.assert_almost_equal(result, 0.36, decimal=5)
 
     def test_dot_batch(self):
         result = quaternion.dot([
-            [1.,0.,0.,0.],
-            [0.,1.,0.,0.],
-            [.2,.2,0.,0.]
-        ],[
-            [0.,1.,0.,0.],
-            [0.,1.,0.,0.],
-            [2.,-.2,0.,0.]
+            [1., 0., 0., 0.],
+            [0., 1., 0., 0.],
+            [.2, .2, 0., 0.]
+        ], [
+            [0., 1., 0., 0.],
+            [0., 1., 0., 0.],
+            [2., -.2, 0., 0.]
         ])
-        expected = [0.,1.,0.36]
+        expected = [0., 1., 0.36]
         np.testing.assert_almost_equal(result, expected, decimal=5)
 
     def test_conjugate(self):
         #result = quaternion.conjugate([5.77350000e-01, 5.77350000e-01, 5.77350000e-01, 6.12323400e-17])
-        result = quaternion.conjugate([0.,0.,0.,1.])
-        np.testing.assert_almost_equal(result, [0.,0.,0.,1.], decimal=5)
+        result = quaternion.conjugate([0., 0., 0., 1.])
+        np.testing.assert_almost_equal(result, [0., 0., 0., 1.], decimal=5)
 
     def test_conjugate_rotation(self):
         result = quaternion.conjugate([5.77350000e-01, 5.77350000e-01, 5.77350000e-01, 6.12323400e-17])
@@ -210,68 +211,71 @@ class test_quaternion(unittest.TestCase):
         pass
 
     def test_inverse(self):
-        result = quaternion.inverse([0.,0.,0.,1.])
-        np.testing.assert_almost_equal(result, [0.,0.,0.,1.], decimal=5)
+        result = quaternion.inverse([0., 0., 0., 1.])
+        np.testing.assert_almost_equal(result, [0., 0., 0., 1.], decimal=5)
 
     def test_inverse_rotation(self):
         result = quaternion.inverse([5.77350000e-01, 5.77350000e-01, 5.77350000e-01, 6.12323400e-17])
         np.testing.assert_almost_equal(result, [-0.577351, -0.577351, -0.577351, 6.12324e-17], decimal=5)
 
     def test_inverse_non_unit(self):
-        q = [1,2,3,4]
+        q = [1, 2, 3, 4]
         result = quaternion.inverse(q)
         expected = quaternion.conjugate(q) / quaternion.length(q)
         np.testing.assert_almost_equal(result, expected, decimal=5)
 
     def test_negate_unit(self):
-        result = quaternion.negate([0.,0.,0.,1.])
-        np.testing.assert_almost_equal(result, [0.,0.,0.,-1.], decimal=5)
+        result = quaternion.negate([0., 0., 0., 1.])
+        np.testing.assert_almost_equal(result, [0., 0., 0., -1.], decimal=5)
 
     def test_negate(self):
-        result = quaternion.negate([1.,2.,3.,4.])
-        np.testing.assert_almost_equal(result, [-1.,-2.,-3.,-4.], decimal=5)
+        result = quaternion.negate([1., 2., 3., 4.])
+        np.testing.assert_almost_equal(result, [-1., -2., -3., -4.], decimal=5)
 
     def test_apply_to_vector_unit_x(self):
-        result = quaternion.apply_to_vector([0.,0.,0.,1.],[1.,0.,0.])
-        np.testing.assert_almost_equal(result, [1.,0.,0.], decimal=5)
+        result = quaternion.apply_to_vector([0., 0., 0., 1.], [1., 0., 0.])
+        np.testing.assert_almost_equal(result, [1., 0., 0.], decimal=5)
 
     def test_apply_to_vector_x(self):
         quat = quaternion.create_from_x_rotation(np.pi / 2.)
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[1.,0.,0.]), [1.,0.,0.]))
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[0.,1.,0.]), [0.,0.,-1.]))
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[0.,0.,1.]), [0.,1.,0.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [1., 0., 0.]), [1., 0., 0.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [0., 1., 0.]), [0., 0., -1.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [0., 0., 1.]), [0., 1., 0.]))
 
     def test_apply_to_vector_y(self):
         quat = quaternion.create_from_y_rotation(np.pi / 2.)
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[1.,0.,0.]), [0.,0.,1.]))
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[0.,1.,0.]), [0.,1.,0.]))
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[0.,0.,1.]), [-1.,0.,0.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [1., 0., 0.]), [0., 0., 1.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [0., 1., 0.]), [0., 1., 0.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [0., 0., 1.]), [-1., 0., 0.]))
 
     def test_apply_to_vector_z(self):
         quat = quaternion.create_from_z_rotation(np.pi / 2.)
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[1.,0.,0.]), [0.,-1.,0.]))
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[0.,1.,0.]), [1.,0.,0.]))
-        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat,[0.,0.,1.]), [0.,0.,1.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [1., 0., 0.]), [0., -1., 0.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [0., 1., 0.]), [1., 0., 0.]))
+        self.assertTrue(np.allclose(quaternion.apply_to_vector(quat, [0., 0., 1.]), [0., 0., 1.]))
 
     def test_identity(self):
         # https://en.wikipedia.org/wiki/Quaternion
         i = quaternion.create(1., 0., 0., 0.)
         j = quaternion.create(0., 1., 0., 0.)
         k = quaternion.create(0., 0., 1., 0.)
+        one = quaternion.create(0., 0., 0., 1.)
 
-        # result = self
-        i1 = quaternion.cross(i, quaternion.create(1., 1., 1., 1.))
-        j1 = quaternion.cross(j, quaternion.create(1., 1., 1., 1.))
-        k1 = quaternion.cross(k, quaternion.create(1., 1., 1., 1.))
-        _1i = quaternion.cross(quaternion.create(1., 1., 1., 1.), i)
-        _1j = quaternion.cross(quaternion.create(1., 1., 1., 1.), j)
-        _1k = quaternion.cross(quaternion.create(1., 1., 1., 1.), k)
+        # i * 1 = i
+        # j * 1 = j
+        # k * 1 = k
+        # 1 * i = i
+        # 1 * j = j
+        # 1 * k = k
+        i1 = quaternion.cross(i, one)
+        j1 = quaternion.cross(j, one)
+        k1 = quaternion.cross(k, one)
+        _1i = quaternion.cross(one, i)
+        _1j = quaternion.cross(one, j)
+        _1k = quaternion.cross(one, k)
 
-        print(i1, _1i)
         self.assertTrue(np.allclose(i1, _1i, i))
-        print(j1, _1j)
         self.assertTrue(np.allclose(j1, _1j, j))
-        print(k1, _1k)
         self.assertTrue(np.allclose(k1, _1k, k))
 
         # result = -1
@@ -280,26 +284,40 @@ class test_quaternion(unittest.TestCase):
         jj = quaternion.cross(j, j)
         ijk = quaternion.cross(quaternion.cross(i, j), k)
 
-        print(ii, jj, kk, ijk)
-        self.assertTrue(np.allclose(ii, jj, kk, ijk, [-1., -1., -1., -1.]))
+        self.assertTrue(np.allclose(ii, -one))
+        self.assertTrue(np.allclose(jj, -one))
+        self.assertTrue(np.allclose(kk, -one))
+        self.assertTrue(np.allclose(ijk, -one))
 
-        # result = k
+        # ij = k
+        # ji = -k
+        # jk = i
+        # kj = -i
+        # ki = j
+        # ik = -j
+
         ij = quaternion.cross(i, j)
-        # result = -k
         ji = quaternion.cross(j, i)
-        # result = i
         jk = quaternion.cross(j, k)
-        # result = -i
         kj = quaternion.cross(k, j)
+        ki = quaternion.cross(k, i)
+        ik = quaternion.cross(i, k)
 
-        print(ij)
         self.assertTrue(np.allclose(ij, k))
-        print(ji)
         self.assertTrue(np.allclose(ji, -k))
-        print(jk)
         self.assertTrue(np.allclose(jk, i))
-        print(kj)
         self.assertTrue(np.allclose(kj, -i))
+        self.assertTrue(np.allclose(ki, j))
+        self.assertTrue(np.allclose(ik, -j))
+
+        # -k = ijkk = ij(k^2) = ij(-1)
+
+        ijkk = quaternion.cross(quaternion.cross(ij, k), k)
+        ijk2 = quaternion.cross(ij, quaternion.cross(k, k))
+        ij_m1 = quaternion.cross(ij, -one)
+
+        self.assertTrue(np.allclose(ijkk, ijk2))
+        self.assertTrue(np.allclose(ijk2, ij_m1))
 
 
 if __name__ == '__main__':