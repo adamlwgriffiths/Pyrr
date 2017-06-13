32c70443f0da5017e9df487115cd1bb26fdede84
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-06-13 21:27:11 +0200

    pep8 a few lines

--- pyrr/matrix44.py
+++ pyrr/matrix44.py
@@ -379,9 +379,9 @@ def create_look_at(eye, target, up, dtype=None):
     up = vector.normalise(np.cross(forward, side))
 
     return np.array((
-            (side[0],            up[0],            forward[0],           0.),
-            (side[1],            up[1],            forward[1],           0.),
-            (side[2],            up[2],            forward[2],           0.),
+            (side[0], up[0], forward[0], 0.),
+            (side[1], up[1], forward[1], 0.),
+            (side[2], up[2], forward[2], 0.),
             (-np.dot(side, eye), -np.dot(up, eye), -np.dot(forward, eye) , 1.0)
         ), dtype=dtype)
         
