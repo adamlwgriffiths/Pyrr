Expanded unit test and fixed two broadcasting bugs along the way

pyrr/matrix44.py
@@ -504,7 +504,7 @@ def decompose(m):
     """
     m = np.asarray(m)
 
-    scale = np.linalg.norm(m[:3, :3], axis=0)
+    scale = np.linalg.norm(m[:3, :3], axis=1)
 
     det = np.linalg.det(m)
     if det < 0:
@@ -512,6 +512,6 @@ def decompose(m):
 
     position = m[3, :3]
 
-    rotation = m[:3, :3] * (1 / scale)
+    rotation = m[:3, :3] * (1 / scale)[:, None]
 
     return scale, quaternion.create_from_matrix(rotation), position

pyrr/tests/objects/test_matrix44.py
@@ -281,36 +281,55 @@ class test_object_matrix44(unittest.TestCase):
         self.assertEqual(m[0,0], 2)
 
     def test_decompose(self):
-        # define expectations
-        expected_scale = Vector3([1, 1, 2], dtype='f4')
-        expected_rotation = Quaternion.from_y_rotation(np.pi, dtype='f4')
-        expected_translation = Vector3([10, 0, -5], dtype='f4')
-        expected_model = Matrix44([
-            [-1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, -2, 0],
-            [10, 0, -5, 1],
-        ], dtype='f4')
+        # define expectations for multiple cases
+        testsets = [
+            (
+                Vector3([1, 1, 2], dtype='f4'),
+                Quaternion.from_y_rotation(np.pi, dtype='f4'),
+                Vector3([10, 0, -5], dtype='f4'),
+                Matrix44([
+                    [-1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, -2, 0],
+                    [10, 0, -5, 1],
+                ], dtype='f4')
+            ),
+            (
+                Vector3([-1, 3, .5], dtype='f4'),
+                Quaternion.from_axis_rotation(Vector3([.75, .75, 0], dtype='f4').normalized, np.pi, dtype='f4').normalized,
+                Vector3([1, -1, 1], dtype='f4'),
+                Matrix44([
+                    [0, -1, 0, 0],
+                    [3, 0, 0, 0],
+                    [0, 0, -.5, 0],
+                    [1, -1, 1, 1],
+                ], dtype='f4')
+            ),
+        ]
 
-        # compose matrix using Pyrr
-        s = Matrix44.from_scale(expected_scale, dtype='f4')
-        r = Matrix44.from_quaternion(expected_rotation, dtype='f4')
-        t = Matrix44.from_translation(expected_translation, dtype='f4')
-        model = t * r * s
-        np.testing.assert_almost_equal(model, expected_model)
-        self.assertTrue(model.dtype == expected_model.dtype)
+        for expected_scale, expected_rotation, expected_translation, expected_model in testsets:
+            # compose model matrix using original inputs
+            s = Matrix44.from_scale(expected_scale, dtype='f4')
+            r = Matrix44.from_quaternion(expected_rotation, dtype='f4')
+            t = Matrix44.from_translation(expected_translation, dtype='f4')
+            m = t * r * s
 
-        # decompose matrix
-        scale, rotation, translation = model.decompose()
-        np.testing.assert_almost_equal(scale, expected_scale)
-        self.assertTrue(scale.dtype == expected_scale.dtype)
-        self.assertTrue(isinstance(scale, Vector3))
-        np.testing.assert_almost_equal(rotation, expected_rotation)
-        self.assertTrue(rotation.dtype == expected_rotation.dtype)
-        self.assertTrue(isinstance(rotation, Quaternion))
-        np.testing.assert_almost_equal(translation, expected_translation)
-        self.assertTrue(translation.dtype == expected_translation.dtype)
-        self.assertTrue(isinstance(translation, Vector3))
+            # check that it's the same as the expected matrix
+            np.testing.assert_almost_equal(m, expected_model)
+            self.assertTrue(m.dtype == expected_model.dtype)
+            self.assertTrue(isinstance(m, expected_model.__class__))
+
+            # decompose this matrix and recompose the model matrix from the decomposition
+            ds, dr, dt = m.decompose()
+            ds = Matrix44.from_scale(ds, dtype='f4')
+            dr = Matrix44.from_quaternion(dr, dtype='f4')
+            dt = Matrix44.from_translation(dt, dtype='f4')
+            dm = dt * dr * ds
+
+            # check that it's the same as the original matrix
+            np.testing.assert_almost_equal(m, dm)
+            self.assertTrue(m.dtype == dm.dtype)
+            self.assertTrue(isinstance(dm, m.__class__))
 
 if __name__ == '__main__':
     unittest.main()